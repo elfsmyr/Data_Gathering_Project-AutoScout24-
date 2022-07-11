b70393483edb83a15d3443eab5d7b27cea6f3213
elfsmyr <elfsmyrshn@gmail.com>
2022-07-11 14:36:44 +0200

    last version

--- autoproject/autoproject/spiders/auto.py
+++ autoproject/autoproject/spiders/auto.py
@@ -9,11 +9,7 @@ class AutoSpider(scrapy.Spider):
                                                                             
 
     def parse(self, response):
-        # print('==========================================================')
         hrefs=response.css("div.ListItem_header__uPzec a ::attr(href)").getall()
-        # print(len(hrefs))
-        # print('==========================================================')
-
         for href in hrefs:
             # print(href)
             url=response.urljoin(href)
@@ -21,34 +17,30 @@ class AutoSpider(scrapy.Spider):
             yield scrapy.Request(url, callback=self.parse_page)
         
         next_page=response.css("li.prev-next ::attr(href)")
-        if next_page:            
-            url= response.urljoin(next_page.get())
-            print('==========================================================')
-            print(url)
-            print('----------------------------------------------')
-            yield scrapy.Request(url,callback=self.parse)
+        for i in range(2,21):
+            if next_page:            
+                url= response.urljoin(next_page.get()) 
+                url=url+str(i)
+                yield scrapy.Request(url,callback=self.parse)
     
-
 
     def parse_page(self, response):
             brand=response.css("div.css-11siofd.errr7t01 span ::text").get()
             brand1=response.css("div.css-11siofd.errr7t01 span ::text")[1].get()
             model=response.css("div.css-l08njs ::text").get()
-            price= response.css("span.StandardPrice_price__X_zzU ::text").get()
+            model=brand1+model
+            price=response.css("span.StandardPrice_price__X_zzU ::text").get()
+            price=price.split(' ')
+            price=price[1]
+            price=price.replace(',-',' €')
             km=response.css("div.VehicleOverview_containerMoreThanFourItems__QgCWJ ::text").extract()[1]
             year=response.css("div.VehicleOverview_containerMoreThanFourItems__QgCWJ ::text").extract()[5]
             city=response.css("a.scr-link.Department_link__6hDp5 div ::text").extract()[3]
+            city=city.split(" ")
+            city=city[2]
+            city=city.replace(',',' ')
             plate=response.css("input.css-11jwkmo ::attr(placeholder)").getall()[2]
-            yield {"brand":brand, "model":model,"price":price,"km":km,"year":year,"city":city,"plate":plate}
-    
-            # next_page=response.css("li.prev-next a ::attr(href)")
-            # print('----------------------------------------------------')
-            # print(next_page)
-            # # next_page=response.css("li.prev-next a").attrib['href']
-            # if next_page is not None:
-            #     yield response.follow(next_page,callback=self.parse)
-
-    
-
-        
-           
+            if plate=="":
+                plate='not found'
+            image=response.css("picture.css-1uafc8p.e3j2jx20 img ::attr(src)").get()
+            yield {"brand":brand, "model":model,"price":price,"km":km,"year":year,"city":city,"plate":plate, "image":image}
